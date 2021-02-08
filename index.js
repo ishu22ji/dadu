1111801fe5bc56616a20854677c35d8241d91d1f
ishu22ji <78218217+ishu22ji@users.noreply.github.com>
2021-02-07 19:45:10 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+    host: 'ishuserver.aternos.me',
+    port: '59723',
+    username: 'hello',
+})
+
+bot.on('login',function(){
+	console.log("Logged In")
+	bot.chat("register qwer qwer");
+})
